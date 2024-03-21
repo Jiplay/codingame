91670f9800fbc3985c1749bcb0a377be34a614e1
Julien Garsia <julien.garsia@epitech.eu>
2024-03-21 15:04:39 +0100

    ✨ Add Coding game Temperature in Go

---
+++ temperature/temperature.go
@@ -0,0 +1,39 @@
+package temperature
+
+import (
+	"strconv"
+)
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func Abs(x int64) int64 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func FindClosestTo0(input []string) string {
+	var lower int64 = 100
+	var index int
+
+	if len(input) == 0 {
+		return "0"
+	}
+
+	for it, inputs := range input {
+		t, _ := strconv.ParseInt(inputs, 10, 32)
+		if Abs(t) < lower {
+			lower = Abs(t)
+			index = it
+		} else if Abs(t) == lower && t > 0 {
+			lower = Abs(t)
+			index = it
+		}
+		_ = t
+	}
+	return input[index]
+}
---
+++ temperature/temperature_test.go
@@ -0,0 +1,34 @@
+package temperature
+
+import "testing"
+
+type Test struct {
+	in  []string
+	out string
+}
+
+var tests = []Test{
+	{[]string{"8", "22", "-33", "12", "-25", "-3", "-24", "-1"}, "-1"},
+	{[]string{"-8", "-22", "-33", "-12", "-25", "-3", "-24", "-1"}, "-1"},
+	{[]string{"-5", "40", "-2", "-12", "44", "-3", "-24", "-1"}, "-1"},
+	{[]string{"-5", "40", "-2", "-12", "44", "-3", "-24", "-1", "1"}, "1"},
+	{[]string{"-5", "40", "-4", "-12", "44", "-3", "-24", "-3", "3"}, "3"},
+	{[]string{}, "0"},
+}
+
+func TestFindClosestTo0(t *testing.T) {
+	for i, test := range tests {
+		closestTo0 := FindClosestTo0(test.in)
+		if closestTo0 != test.out {
+			t.Errorf("#%d: Taille(%s)=%s; attendu %s", i, test.in, closestTo0, test.out)
+		}
+	}
+}
+
+/*
+
+* go test; permet de lancer les tests
+* go test -coverprofile=coverage.out; permet de créer coverage.out
+* go tool cover -html=coverage.out; permet de visualiser le coverage
+
+ */
